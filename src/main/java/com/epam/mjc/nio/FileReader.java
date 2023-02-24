9067e5ede5ce6241bb15fe827f02bf0e08744865
epampam <epampam@wp.pl>
2023-02-24 20:57:40 +0100

    finished

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,8 +1,6 @@
 package com.epam.mjc.nio;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,28 +10,33 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
-
-        try {
-            Path path = file.toPath();
-            BufferedReader reader = Files.newBufferedReader(path);
-
-            profile.setName(reader.readLine().substring(6).trim());
-            profile.setAge(Integer.parseInt(reader.readLine().substring(5).trim()));
-            profile.setEmail(reader.readLine().substring(7).trim());
-            profile.setPhone(Long.parseLong(reader.readLine().substring(7).trim()));
-
-            reader.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + file.getAbsolutePath());
-            return null;
+        Path path = file.toPath();
+        try (var reader = Files.newBufferedReader(path)) {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                var entry = line.split(": ");
+                var key = entry[0];
+                var value = entry[1];
+                switch (key) {
+                    case "Name":
+                        profile.setName(value);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.valueOf(value));
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.valueOf(value));
+                        break;
+                    case "Email":
+                        profile.setEmail(value);
+                        break;
+                    default:
+                        throw new IllegalStateException("Uknown key: " + key);
+                }
+            }
+            return profile;
         } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-            return null;
-        } catch (Exception e) {
-            System.err.println("Unexpected error: " + e.getMessage());
-            return null;
-        }
 
-        return profile;
+            throw new IllegalArgumentException(e);
+        }
     }
 }
